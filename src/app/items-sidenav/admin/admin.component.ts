7c9e97c658503e7ef407f45ec48ad5a0e20526f5
Luis <luiscoba88@yahoo.com>
2020-09-17 19:59:20 -0500

    se agrega la carga de la base a 'admin'

--- src/app/items-sidenav/admin/admin.component.ts
+++ src/app/items-sidenav/admin/admin.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from "@angular/core";
+import { NgxSpinnerService } from "ngx-spinner";
+import { ParamService } from "../../dashboard/param.service";
+import { ProcessIDB } from "../../shared/bd/process.indexedDB";
+import { NgxIndexedDBService } from "ngx-indexed-db";
 
 @Component({
   selector: "app-admin",
@@ -6,7 +10,34 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./admin.component.css"],
 })
 export class AdminComponent implements OnInit {
-  constructor() {}
+  processIDB: ProcessIDB;
+  constructor(
+    private paramService: ParamService,
+    private dbService: NgxIndexedDBService,
+    private spinner: NgxSpinnerService
+  ) {
+    this.processIDB = new ProcessIDB(dbService);
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    // carga de los parametros de la app
+    this.loadAllParameters();
+  }
+
+  loadAllParameters() {
+    this.spinner.show();
+
+    this.paramService.getParameters().subscribe(
+      (allParameters) => {
+        console.log("allParameterssssssssss", allParameters);
+        // llenamos la base 'indexed-db'
+        this.processIDB.fillingParameters(allParameters);
+        this.spinner.hide();
+      },
+      (error) => {
+        console.log(error);
+        this.spinner.hide();
+      }
+    );
+  }
 }
